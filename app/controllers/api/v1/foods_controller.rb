76d803618722c1f07f7c6b4f69c76cabb7ec33d1
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-06 17:34:11 +0200

    Add stuffs

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+
+    render json: {status:"success", data: {foods: @foods} }
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      render json: {status: 'success', data: {food: @food}}, status: :created
+    else
+      render json: {status: 'fail', error: {message: "Couldn't create user"}}, status: :fail
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :created_by, :description, :quantity)
+  end
 end
