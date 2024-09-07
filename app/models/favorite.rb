dc8f26f80949e356f80e7a3c4b35f7aa5c36a214
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-07 18:01:17 +0200

    Add foods

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
 class Favorite < ApplicationRecord
-  belongs_to :user, foreign_key: :user_id, dependent: :destroy
-  belongs_to :food, foreign_key: :food_id, dependent: :destroy
+  belongs_to :user, foreign_key: :user_id
+  belongs_to :food, foreign_key: :food_id
 end
